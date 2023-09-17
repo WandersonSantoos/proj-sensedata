c2f274fcf08309991c4539e6bef6a690f5acfa35
Wanderson.jpeg <81868579+WandersonSantoos@users.noreply.github.com>
2023-09-17 20:03:53 -0300

    Atualização no Readmi
    Algumas correções

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # proj-sensedata
  Teste estágio Dev-Mobile SenseData
+
+# Projeto de Gestão de Gastos Pessoais
+
+Bem-vindo ao Projeto de Gestão de Gastos Pessoais! Este é um projeto de demonstração que inclui um aplicativo web simples para controlar suas finanças pessoais. Você pode acessá-lo [aqui](https://wandersonsantoos.github.io/proj-sensedata/).
+
+## Acesso ao Aplicativo
+
+Para acessar o aplicativo, utilize as seguintes credenciais:
+
+- **Login:** sensedata@gmail.com
+- **Senha:** zenvia123
+
+## Tecnologias Utilizadas
+
+Este projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+## Páginas Disponíveis
+
+O aplicativo inclui as seguintes páginas:
+
+1. **Login:** Página de login onde você pode inserir suas credenciais para acessar o painel de controle financeiro.
+
+2. **Home:** Página principal após o login, onde você pode visualizar informações sobre suas finanças pessoais, adicionar novos itens e ver mudanças recentes.
+
+3. **Error:** Página de erro que é exibida se você acessar uma URL incorreta ou não encontrada.
+
+4. **Construção:** Página temporária que indica que determinada funcionalidade ainda está em desenvolvimento.
+
+## Como Utilizar
+
+1. Faça login utilizando as credenciais fornecidas.
+2. Na página inicial (Home), você pode ver informações sobre suas entradas, saídas e o total.
+3. Você pode adicionar novos itens financeiros na seção "Incluir".
+4. As mudanças recentes são exibidas na parte inferior da página principal.
+5. Explore outras páginas, como Error e Construção, para ver páginas de exemplo.
+
+## Contribuições
+
+Contribuições para este projeto são bem-vindas! Sinta-se à vontade para abrir problemas (issues) e enviar pull requests com melhorias ou correções.
+
+## Licença
+
+Este projeto é licenciado sob a Licença MIT - consulte o arquivo [LICENSE](LICENSE) para obter detalhes.
+
+---
+
+**Aproveite o aplicativo de Gestão de Gastos Pessoais!** 💰
--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona os elementos do formulário
     const usuarioInput = document.getElementById('usuario');
     const senhaInput = document.getElementById('senha');
     const loginButton = document.getElementById('loginButton');
@@ -6,86 +7,79 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector('.login_button')
     let validarLogin = false;
 
+    // Exibe um alerta com informações de login
+    alert("Para acessar o dashboard, utilize: sensedata@gmail.com e zenvia123")
+
+    // Adiciona um evento de clique ao botão de login
     loginButton.addEventListener('click', fazerLogin);
 
+    // Função para processar o login
     function fazerLogin() {
+        // Array de usuários permitidos com login e senha correspondentes
         const usuarios = [
             {
                 login: 'wandersonsantos.ws51@gmail.com',
                 pass: 'devsensdata',
             },
             {
+                login: 'sensedata@gmail.com',
+                pass: 'zenvia123',
+            },
+            {
                 login: 'shayannefideliz.sf17@gmail.com',
                 pass: 'loki12345',
             },
-            {
-                login: 'joao',
-                pass: '1234',
-            },
-            {
-                login: 'Arthur',
-                pass: '1234',
-            },
-            {
-                login: 'nancy',
-                pass: '1234',
-            },
-            {
-                login: 'julia',
-                pass: '1234',
-            },
-            {
-                login: 'admin',
-                pass: 'admin',
-            },
         ];
 
-
+        // Verifica se as credenciais fornecidas coincidem com algum usuário
         for (let i = 0; i < usuarios.length; i++) {
             if (usuarioInput.value === usuarios[i].login && senhaInput.value === usuarios[i].pass) {
-                localStorage.setItem('usuarioLogado', 'true'); // Armazena um valor no localStorage
+                localStorage.setItem('usuarioLogado', 'true'); // Armazena um valor no localStorage para indicar que o usuário está logado
                 validarLogin = true;
                 window.location.href = 'pages/home.html'; // Redireciona para a página home.html após o login bem-sucedido
                 return;
             }
         }
 
+        // Exibe uma mensagem de erro se as credenciais estiverem incorretas
         alert('Usuário ou senha incorretos!');
         senhaInput.value = '';
         usuarioInput.value = '';
     }
     
+    // Adiciona eventos de input para validar o formulário
     usuarioInput.addEventListener('input', validarFormulario);
     senhaInput.addEventListener('input', validarFormulario);
 
+    // Função para validar o formulário com base nos campos preenchidos
     function validarFormulario() {
         const usuario = usuarioInput.value;
         const senha = senhaInput.value;
 
         if (usuario && senha.length >= 8) {
-            loginButton.removeAttribute('disabled');
+            loginButton.removeAttribute('disabled'); // Habilita o botão de login se as condições forem atendidas
         } else {
-            loginButton.setAttribute('disabled', '');
+            loginButton.setAttribute('disabled', ''); // Desabilita o botão de login se as condições não forem atendidas
         }
     }
 
+    // Função para lidar com o foco nos campos de entrada
     const handleFocus = ({ target }) => {
         const span = target.previousElementSibling;
-        span.classList.add('span-active');
+        span.classList.add('span-active'); // Adiciona uma classe para realçar o rótulo quando o campo está em foco
     }
     
+    // Função para lidar com a perda de foco nos campos de entrada
     const handleFocusOut = ({ target }) => {
     
         if(target.value == '') {
             const span = target.previousElementSibling;
-            span.classList.remove('span-active');
+            span.classList.remove('span-active'); // Remove a classe que realça o rótulo quando o campo perde o foco e está vazio
         }
     }
     
+    // Adiciona eventos de foco, foco perdido e entrada para todos os campos de entrada
     inputs.forEach((input) => {input.addEventListener('focus', handleFocus)});
-    
     inputs.forEach((input) => {input.addEventListener('focusout', handleFocusOut)});
-    
     inputs.forEach((input) => {input.addEventListener('input', handleChange)});
-    
 });
